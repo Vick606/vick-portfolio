6f752a538fe5e17f117d5492520f04dc322875c1
Vick K <vickoch20@gmail.com>
2025-01-06 12:56:47 +0300

    update header info

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -74,7 +74,7 @@ export function Header() {
                 handleNavClick('#home')
               }}
             >
-              Moncef Djezzar
+              Victor Koech
             </Link>
           </motion.div>
 
